499307ed86c9fedb5b67dbb80a587b39a898c00c
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-10-17 23:00:43 -0700

    added spotify, movie-this function

--- keys.js
+++ keys.js
@@ -1,6 +1,11 @@
 console.log('this is loaded');
 
-exports.spotify = {
+// process the "dotenv" package and extract info from .env file
+require("dotenv").config();
+
+var spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+module.exports = spotify;
--- liri.js
+++ liri.js
@@ -1,13 +1,3 @@
-// not sure what exactly it is??????????/
-require("dotenv").config();
-
-// get spotify keys from keys.js
-var keys = require("./keys");
-// get node spotify package
-var Spotify = require("node-spotify-api");
-// pass the object into node spotify package
-var spotify = new Spotify(keys.spotify);
-
 // get moment package
 var moment = require("moment");
 
@@ -51,13 +41,121 @@ switch(command) {
         break;
 
     case "spotify-this-song":
-        // something
+        // condense user inputs
+        var usrinput = userinputArr.join("+");
+        // get node spotify package
+        var Spotify = require("node-spotify-api");
+        // get spotify keys from keys.js
+        var spotifyKeys = require("./keys");
+        // pass the object into node spotify package
+        var spotify = new Spotify(spotifyKeys);
+
+        if(usrinput !== "") {
+            spotify
+                .search({type:'track', query: usrinput})
+                .then(function(response) {
+                    for(var i = 0; i < response.tracks.items.length; i++) {
+                        console.log("\n--------- Here are searching results --------\n");
+                        console.log("Song name: " + response.tracks.items[i].name);
+                        console.log("Artist: " + response.tracks.items[i].artists[0].name);
+                        console.log("Preview link: " + response.tracks.items[i].external_urls.spotify);
+                        console.log("Album name: " + response.tracks.items[i].album.name);
+                    }
+                })
+                .catch(function(err) {
+                    console.log("\n---------- Error ----------\n");
+                    console.log("Error: " + err);
+                });
+        }
+
+        else {
+            spotify
+                .search({type:'track', query: 'The Sign'})
+                .then(function(response) {
+                    for(var i = 0; i < response.tracks.items.length; i++) {
+                        if(response.tracks.items[i].artists[0].name === "Ace of Base") {
+                            console.log("Song name: " + response.tracks.items[i].name);
+                            console.log("Artist: " + response.tracks.items[i].artists[0].name);
+                            console.log("Preview link: " + response.tracks.items[i].external_urls.spotify);
+                            console.log("Album name: " + response.tracks.items[i].album.name);
+                        }
+
+                    }
+                })
+        }
 
 
         break;
 
     case "movie-this":
-        // something
+        // condense user inputs
+        var usrinput = userinputArr.join(" ");
+
+        if(usrinput !== "") {
+            // make request
+            var request = require("request");
+            // make query
+            var queryUrl = "http://www.omdbapi.com/?t=" + usrinput + "&apikey=trilogy";
+
+            request(queryUrl, function(err, response, body) {
+
+                var title = JSON.parse(body).Title;
+                var year = JSON.parse(body).Year;
+                var imdbRating = JSON.parse(body).imdbRating;
+                var rotten = JSON.parse(body).Ratings[1].Value;
+                var country = JSON.parse(body).Country;
+                var language = JSON.parse(body).Language;
+                var plot = JSON.parse(body).Plot;
+                var actors = JSON.parse(body).Actors;
+
+                if(!err && response.statusCode === 200) {
+                    console.log("\n-------------------\n");
+                    console.log("Title: " + title);
+                    console.log("Year came out: " + year);
+                    console.log("imdb Rating: " + imdbRating);
+                    console.log("Rotten tomatoes Rating: " + rotten);
+                    console.log("Country: " + country);
+                    console.log("Language: " + language);
+                    console.log("Plot: \n" + plot);
+                    console.log("Actors: \n" + actors);
+                }
+            })
+        }
+        else {
+            console.log("\n------------\n");
+            console.log("If you haven't watched 'Mr.Nobody', then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+
+            // make request
+            var request = require("request");
+            // make query
+            var errQueryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&apikey=trilogy";
+
+            request(errQueryUrl, function(err, response, body) {
+
+                var title = JSON.parse(body).Title;
+                var year = JSON.parse(body).Year;
+                var imdbRating = JSON.parse(body).imdbRating;
+                var rotten = JSON.parse(body).Ratings[1].Value;
+                var country = JSON.parse(body).Country;
+                var language = JSON.parse(body).Language;
+                var plot = JSON.parse(body).Plot;
+                var actors = JSON.parse(body).Actors;
+
+                if(!err && response.statusCode === 200) {
+                    console.log("\n-------------------\n");
+                    console.log("Title: " + title);
+                    console.log("Year came out: " + year);
+                    console.log("imdb Rating: " + imdbRating);
+                    console.log("Rotten tomatoes Rating: " + rotten);
+                    console.log("Country: " + country);
+                    console.log("Language: " + language);
+                    console.log("Plot: \n" + plot);
+                    console.log("Actors: \n" + actors);
+                }
+            })
+        }
+
         break;
     
     case "do-what-it-says":
